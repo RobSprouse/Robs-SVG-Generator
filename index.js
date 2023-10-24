5cc9ad5171ea8be61d258837acca20a44011f3a6
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-10-24 11:33:06 -0400

    completed prompts
    completed prompts

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+const questions = [
+     {
+          type: "input",
+          name: "text",
+          message: "Enter at least one to three characters",
+          validate: function (value) {
+               if (value.length > 0 && value.length <= 3) {
+                    return true;
+               }
+               return "Please enter at least one to three characters.";
+          },
+     },
+     {
+          type: "input",
+          name: "textColor",
+          message: "Enter a color keyword or a hexadecimal number for the text color.",
+     },
+     {
+          type: "list",
+          name: "shape",
+          message: "Choose a shape from the list",
+          choices: ["circle", "triangle", "square"],
+     },
+     {
+          type: "input",
+          name: "shapeColor",
+          message: "Enter a color keyword or a hexadecimal number for the shape's color.",
+     },
+];
